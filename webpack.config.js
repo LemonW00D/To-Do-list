2162b43145f4cbd7276e45fff3345d9dce7b4b0e
LemonW00D <arnaud.tessier37@hotmail.fr>
2020-07-15 21:06:53 +0200

    Projet fini

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/i,
+        use:[
+          "style-loader", "css-loader"
+          ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html")
+    })
+  ],
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    inline: true,
+    open: true,
+    hot: true,
+    port: 4000
+  }
+};
